ca2a1dfd801ab2c022a42b8a137b40c6e447fa28
songxh0424 <songxh@umich.edu>
2018-05-27 12:37:28 -0400

    added overall player match stats

--- Scraping/CleanData.R
+++ Scraping/CleanData.R
@@ -5,8 +5,11 @@ library(stringr)
 
 load('../Data/allMatchStats.RData')
 
+finals = c('2625', '3141', '2624', '2510', '2509', '2454', '2817')
+idx = which(names(allMatchStats) %in% finals)
+
 ## bind all stats tables into a large table
-detailedStats = lapply(allMatchStats, function(match) {
+detailedStats = lapply(allMatchStats[-idx], function(match) {
   date = match$date
   team1 = match$team1
   team2 = match$team2
@@ -16,7 +19,8 @@ detailedStats = lapply(allMatchStats, function(match) {
     mutate(Team = if_else(Player %in% players1, team1, team2),
            Opponent = if_else(Player %in% players1, team2, team1),
            Score = if_else(Player %in% players1, paste0(match$score, collapse = '-'),
-                           paste0(rev(match$score), collapse = '-'))) %>%
+                           paste0(rev(match$score), collapse = '-')),
+           Result = if_else(str_sub(Score, end = 1) > str_sub(Score, start = -1), 'Win', 'Lose')) %>%
     select(-Impact)
 }) %>% bind_rows()
 
@@ -28,8 +32,20 @@ detailedStats = detailedStats %>%
 
 save(detailedStats, file = '../Data/detailedStats.RData')
 
-## aggregate player stats over all match, stratified by player and by hero
-## playerStats = detailedStats %>%
-##   mutate(Time = as.numeric(Time) / 60) %>%
-##   group_by(Team, Player, Hero) %>%
-##   summarise(Time = sum(Time), )
+playerStats = lapply(allMatchStats[-idx], function(match) {
+  date = match$date
+
+  df1 = match$playerStats$team1 %>%
+    mutate(Team = match$team1, Opponent = match$team2) %>%
+    select(-`Rating?`)
+  df2 = match$playerStats$team2 %>%
+    mutate(Team = match$team2, Opponent = match$team1) %>%
+    select(-`Rating?`)
+  df1 %>% bind_rows(df2) %>% mutate(Date = mdy(date))
+}) %>% bind_rows()
+
+playerStats = detailedStats %>% group_by(Team, Date, Player) %>%
+  summarise(Time = sum(Time), Score = first(Score), Result = first(Result)) %>%
+  right_join(playerStats, by = c('Team', 'Date', 'Player'))
+
+save(playerStats, file = '../Data/playerStats.RData')
