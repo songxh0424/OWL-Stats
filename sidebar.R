c6ff69d79ea1650ba3c89c067c67c439fe847246
songxh0424 <songxh@umich.edu>
2018-06-17 22:51:00 -0400

    missed a comma

--- sidebar.R
+++ sidebar.R
@@ -26,7 +26,7 @@ dbSidebar = dashboardSidebar(
         br()
       ),
       'heroes'
-    )
+    ),
     ## menuItem('League Overview', tabName = 'league', icon = icon('list')),
     tags$head(tags$script(HTML('$(document).ready(function() {$(".treeview-menu").css("display", "block");})')))
   )
